a4cacb9e0fe2faa900cb1aa13b56a20ace551727
miftah-mj <jannat27120@gmail.com>
2026-01-26 00:41:41 +0600

    Add job and about routes to AppRouter and include job logo

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -12,94 +12,106 @@ import MyApplications from "../pages/Applications/MyApplications";
 import AddJob from "../pages/Jobs/AddJob";
 import MyPostedJobs from "../pages/Jobs/MyPostedJobs";
 import ViewApplications from "../pages/Applications/ViewApplications";
+import Jobs from "../pages/Jobs/Jobs";
+import About from "../pages/About/About";
 
-const AppRouter = createBrowserRouter([
-    {
-        path: "/",
-        element: <MainLayout />,
-        children: [
-            {
-                path: "/",
-                element: <Home />,
-            },
-            {
-                path: "jobs/:id",
-                element: (
-                    <ProtectedRoute>
-                        <Jobdetails />
-                    </ProtectedRoute>
-                ),
-                loader: ({ params }) =>
-                    fetch(`https://jobportal-server-side.vercel.app/jobs/${params.id}`),
-            },
-            {
-                path: "jobs-apply/:id",
-                element: (
-                    <ProtectedRoute>
-                        <JobApply />
-                    </ProtectedRoute>
-                ),
-                loader: ({ params }) =>
-                    fetch(`https://jobportal-server-side.vercel.app/jobs/${params.id}`),
-            },
-            {
-                path: "my-applications",
-                element: (
-                    <ProtectedRoute>
-                        <MyApplications />
-                    </ProtectedRoute>
-                ),
-                // loader: ({params}) =>
-                //     fetch(`/job-applications/email=${user.email}/${params.id}`),
-            },
-            {
-                path: "add-job",
-                element: (
-                    <ProtectedRoute>
-                        <AddJob />
-                    </ProtectedRoute>
-                ),
-            },
-            {
-                path: "my-posted-jobs",
-                element: (
-                    <ProtectedRoute>
-                        <MyPostedJobs />
-                    </ProtectedRoute>
-                ),
-            },
-            {
-                path: "view-applications/:job_id",
-                element: (
-                    <ProtectedRoute>
-                        <ViewApplications />
-                    </ProtectedRoute>
-                ),
-                loader: ({ params }) =>
-                    fetch(
-                        `https://jobportal-server-side.vercel.app/job-applications/jobs/${params.job_id}`
+const AppRouter = createBrowserRouter(
+    [
+        {
+            path: "/",
+            element: <MainLayout />,
+            children: [
+                {
+                    path: "/",
+                    element: <Home />,
+                },
+                {
+                    path: "jobs",
+                    element: <Jobs />,
+                },
+                {
+                    path: "about",
+                    element: <About />,
+                },
+                {
+                    path: "jobs/:id",
+                    element: (
+                        <ProtectedRoute>
+                            <Jobdetails />
+                        </ProtectedRoute>
                     ),
-            },
-        ],
-    },
-    {
-        path: "auth",
-        element: <AuthLayout />,
-        children: [
-            {
-                path: "register",
-                element: <Register />,
-            },
-            {
-                path: "signin",
-                element: <Signin />,
-            },
-        ],
-    },
-    {
-        path: "*",
-        element: <NotFound />,
-    },
-]);
+                    loader: ({ params }) =>
+                        fetch(`https://jobportal-server-side.vercel.app/jobs/${params.id}`),
+                },
+                {
+                    path: "jobs-apply/:id",
+                    element: (
+                        <ProtectedRoute>
+                            <JobApply />
+                        </ProtectedRoute>
+                    ),
+                    loader: ({ params }) =>
+                        fetch(`https://jobportal-server-side.vercel.app/jobs/${params.id}`),
+                },
+                {
+                    path: "my-applications",
+                    element: (
+                        <ProtectedRoute>
+                            <MyApplications />
+                        </ProtectedRoute>
+                    ),
+                    // loader: ({params}) =>
+                    //     fetch(`/job-applications/email=${user.email}/${params.id}`),
+                },
+                {
+                    path: "add-job",
+                    element: (
+                        <ProtectedRoute>
+                            <AddJob />
+                        </ProtectedRoute>
+                    ),
+                },
+                {
+                    path: "my-posted-jobs",
+                    element: (
+                        <ProtectedRoute>
+                            <MyPostedJobs />
+                        </ProtectedRoute>
+                    ),
+                },
+                {
+                    path: "view-applications/:job_id",
+                    element: (
+                        <ProtectedRoute>
+                            <ViewApplications />
+                        </ProtectedRoute>
+                    ),
+                    loader: ({ params }) =>
+                        fetch(
+                            `https://jobportal-server-side.vercel.app/job-applications/jobs/${params.job_id}`
+                        ),
+                },
+            ],
+        },
+        {
+            path: "auth",
+            element: <AuthLayout />,
+            children: [
+                {
+                    path: "register",
+                    element: <Register />,
+                },
+                {
+                    path: "signin",
+                    element: <Signin />,
+                },
+            ],
+        },
+        {
+            path: "*",
+            element: <NotFound />,
+        },
+    ],
+);
 
 export default AppRouter;
